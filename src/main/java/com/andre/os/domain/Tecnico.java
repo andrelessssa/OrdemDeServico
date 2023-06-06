bb4974a43e3ed7ab0261f00a861b83f68e216c2f
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-06-06 19:22:04 -0300

    dominio
    classes de Dominio criada

--- src/main/java/com/andre/os/domain/Tecnico.java
+++ src/main/java/com/andre/os/domain/Tecnico.java
@@ -1,5 +1,12 @@
 package com.andre.os.domain;
 
-public class Tecnico extends Pessoa{
+public class Tecnico extends Pessoa {
 
+    public Tecnico() {
+        super();
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String telefone) {
+        super(id, nome, cpf, telefone);
+    }
 }
